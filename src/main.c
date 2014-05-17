eed164b6990105871ed4c0cf6f3249430ce93c6f
唐成 <tangcheng2005@gmail.com>
2014-05-17 11:16:44 +0800

    新建工程。

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.c
+ * Copyright (C) 2014 TangCheng <tangcheng_@163.com>
+ * 
+ */
+
+#include <stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	return (0);
+}
+
