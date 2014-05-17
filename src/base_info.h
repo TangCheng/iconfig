1466da9a22c056d46f1655cf9844c215e555140d
唐成 <tangcheng2005@gmail.com>
2014-05-18 00:18:26 +0800

    录入错误。

--- src/base_info.h
+++ src/base_info.h
@@ -8,7 +8,7 @@
 #define IPCAM_BASE_INFO_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), IPCAM_BASE_INFO_TYPE, IpcamBaseInfoClass))
 #define IPCAM_IS_BASE_INFO(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), IPCAM_BASE_INFO_TYPE))
 #define IPCAM_IS_BASE_INFO_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), IPCAM_BASE_INFO_TYPE))
-#define IPCAM_BASE_INFO_GET_CLASS(obj) (G_TYPE_GET_INSTANCE_CLASS((ojb), IPCAM_BASE_INFO_TYPE, IpcamBaseInfoClass))
+#define IPCAM_BASE_INFO_GET_CLASS(obj) (G_TYPE_GET_INSTANCE_CLASS((obj), IPCAM_BASE_INFO_TYPE, IpcamBaseInfoClass))
 
 typedef struct _IpcamBaseInfo IpcamBaseInfo;
 typedef struct _IpcamBaseInfoClass IpcamBaseInfoClass;
