698bc99416998034751089dd3d063e97cb64f0b2
TangCheng <tangcheng2005@gmail.com>
2014-10-10 14:46:21 +0800

    修改之前代码拷贝导致多个头文件中都使用错误的“osd”拼写

--- src/ajax/http_video_handler.h
+++ src/ajax/http_video_handler.h
@@ -4,11 +4,11 @@
 #include "http_request_handler.h"
 
 #define IPCAM_HTTP_VIDEO_HANDLER_TYPE (ipcam_http_video_handler_get_type())
-#define IPCAM_HTTP_VIDEO_HANDLER(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttposdHandler))
-#define IPCAM_HTTP_VIDEO_HANDLER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttposdHandlerClass))
+#define IPCAM_HTTP_VIDEO_HANDLER(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttpVideoHandler))
+#define IPCAM_HTTP_VIDEO_HANDLER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttpVideoHandlerClass))
 #define IPCAM_IS_HTTP_VIDEO_HANDLER(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), IPCAM_HTTP_VIDEO_HANDLER_TYPE))
 #define IPCAM_IS_HTTP_VIDEO_HANDLER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), IPCAM_HTTP_VIDEO_HANDLER_TYPE))
-#define IPCAM_HTTP_VIDEO_HANDLER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttposdHandlerClass))
+#define IPCAM_HTTP_VIDEO_HANDLER_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), IPCAM_HTTP_VIDEO_HANDLER_TYPE, IpcamHttpVideoHandlerClass))
 
 typedef struct _IpcamHttpVideoHandler IpcamHttpVideoHandler;
 typedef struct _IpcamHttpVideoHandlerClass IpcamHttpVideoHandlerClass;
